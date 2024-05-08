0e3d8c0f562e989f3b66abda8cb3eab31bab2fc9
literallyapotato <iamdot957@gmail.com>
2024-05-08 09:08:29 -0400

    finished the basic skeleton of the game

--- NLNWRoulette.py
+++ NLNWRoulette.py
@@ -1,7 +1,6 @@
 import random
 
 
-
 class Roulette:
     '''
     Non-Lethal, Non-Weapon roulette game
@@ -9,11 +8,14 @@ class Roulette:
     GAME DESCRIPTION HERE
     '''
     __STARTINGHEALTH = 3
-    __SHELLMIN = 4
-    __SHELLMAX = 8
+    
+    __shells = 5
 
     __chamber = []    #0 represent a blank, 1 represents live
     __turn = 0    #the turn of the player
+                    #player 0 starts, player 1 goes afterwards
+                    #use turn%2 to find the current player
+    
     __hp = [__STARTINGHEALTH, __STARTINGHEALTH] #represents the hp of the two players
 
     def __init__(self):
@@ -23,21 +25,23 @@ class Roulette:
         '''
         load and reset the chamber
         '''    
-        for x in range(random.randint(self.__SHELLMIN, self.__SHELLMAX)):
+        self.__chamber = [0, 1]
+        for x in range(self.__shells - 2):
             self.__chamber.append(random.randint(0,1))
+        random.shuffle(self.__chamber)
+        
 
-    
     def attack(self, attacker:int, target:int) -> int:
         '''
         attacker - the number of the player using the weapon
-        target - the target of the weapon, relative to the attacker
-                0 is self, 1 is opponent
+        target - the number of the player being attacked
         returns 1 if hit, 0 if blank
         '''
         fired = self.__chamber.pop(0) #the shell to be fired
-        self.__turn += target #if attack target is self, do not change turns
-        self.__hp[(attacker+target)%2] -= fired #remove hp from the target. If the shell is blank, 0 hp is removed
+        self.__turn += (attacker != target) #if attack target is self, do not change turns
+        self.__hp[target] -= fired #remove hp from the target. If the shell is blank, 0 hp is removed
         return fired
+    
 
 
 
